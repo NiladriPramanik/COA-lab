cad21023dabd737c12ca1f6ca8998837e227d0d1
NiladriPramanik <107825176+NiladriPramanik@users.noreply.github.com>
2023-02-16 10:11:06 +0530

    Add files via upload

---
+++ Decimal to Binary.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n<=0.0){
+        if(n==0.0){
+        cout<<"Zero"<<endl;
+    }
+    else cout<<"Negative Number"<<endl;
+    }
+    else cout<<"Positive Number"<<endl;
+    return 0;
+}
+
+
+Output
+1001111
+
+Input 
+121
---
+++ Positive Negative Number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n<=0.0){
+        if(n==0.0){
+        cout<<"Zero"<<endl;
+    }
+    else cout<<"Negative Number"<<endl;
+    }
+    else cout<<"Positive Number"<<endl;
+    return 0;
+}
+
+
+Input
+121
+
+Output
+Positive Number
